f4da426cde57a6613578f1d3fb4e6bd7d6864185
Luis Felipe Zaguini <luisfelipezaguini@gmail.com>
2019-10-29 14:57:43 -0300

    fixed tests description

--- test/use-selection-sort.test.ts
+++ test/use-selection-sort.test.ts
@@ -2,7 +2,7 @@ import { renderHook } from '@testing-library/react-hooks';
 import { useSelectionSort } from '../src/use-selection-sort';
 
 describe('useSelectionSort', () => {
-  it('sorts the values correctly', () => {
+  it('sorts the values correctly without modifying the original array', () => {
     const unsortedArray = [64, 25, 12, 22, 11];
     const { result } = renderHook(() => useSelectionSort(unsortedArray));
 
