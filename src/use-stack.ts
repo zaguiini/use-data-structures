c8f49466ac8876349869c13e5e9a07c5bb8c10d1
Weslei Juan Novaes Pereira <wesleimsr@gmail.com>
2019-10-30 10:23:34 -0300

    Added useStack

---
+++ src/use-stack.ts
@@ -0,0 +1,45 @@
+import { useState, useMemo } from 'react';
+
+export const useStack = <T>(initialState: T[] = []) => {
+  const [value, setValue] = useState(initialState);
+
+  const handlers = useMemo(
+    () =>
+      Object.assign(value, {
+        isEmpty: () => {
+          return value.length === 0;
+        },
+
+        peek: () => {
+          let peeked = value[value.length - 1];
+          return peeked;
+        },
+
+        pop: (): void | string => {
+          if (value.length === 0) return;
+
+          let popped;
+          setValue(oldStack => {
+            const newStack = [...oldStack];
+            popped = newStack.pop();
+
+            return newStack;
+          });
+
+          return popped;
+        },
+
+        push: (item: T) => {
+          setValue(oldStack => {
+            const newStack = [...oldStack];
+            newStack.push(item);
+
+            return newStack;
+          });
+        },
+      }),
+    [value]
+  );
+
+  return handlers;
+};
