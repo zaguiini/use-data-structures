397c907c54d3f3212983b9bc57247fdaa99263dd
Luis Felipe Zaguini <luisfelipezaguini@gmail.com>
2019-11-01 10:16:52 -0300

    added docz to readme (#37)

--- doczrc.js
+++ doczrc.js
@@ -1,12 +1,12 @@
 module.exports = {
   title: 'useDataStructures',
   typescript: true,
-  menu: ['useMap', 'useSet', 'Sorting algorithms'],
+  menu: ['What is this?', 'useMap', 'useSet', 'Sorting algorithms'],
   themeConfig: {
     mode: 'dark',
     colors: {
-      primary: "#3BCCA5",
+      primary: '#3BCCA5',
     },
-    linesToScrollEditor: 100
-  }
+    linesToScrollEditor: 100,
+  },
 }
