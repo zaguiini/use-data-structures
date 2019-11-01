41635e59ae66f3833cf8bb1d442302dae42de602
Weslei Juan Novaes Pereira <wesleimsr@gmail.com>
2019-11-01 13:16:00 -0300

    Implemented queue

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 export * from './use-set'
 export * from './use-map'
 export * from './use-stack'
+export * from './use-queue'
 
 export * from './use-selection-sort'
 export * from './use-bubble-sort'
---
+++ src/use-queue.ts
@@ -0,0 +1,46 @@
+import { useState, useMemo } from 'react'
+import produce from 'immer'
+
+export const useQueue = <T>(initialValue: T[] = []) => {
+  const [value, setValue] = useState(initialValue)
+
+  const handlers = useMemo(
+    () => ({
+      dequeue: () => {
+        if (value.length === 0) return
+
+        let shifted
+        setValue((oldQueue) =>
+          produce(oldQueue, (draft: T[]) => {
+            shifted = draft.shift()
+          })
+        )
+
+        return shifted
+      },
+
+      enqueue: (item: T) => {
+        setValue((oldQueue) =>
+          produce(oldQueue, (draft: T[]) => {
+            draft.push(item)
+          })
+        )
+      },
+
+      front: () => {
+        if (value.length === 0) return
+
+        return value[0]
+      },
+
+      isEmpty: () => {
+        return value.length === 0
+      },
+
+      size: value.length,
+    }),
+    [value]
+  )
+
+  return handlers
+}
