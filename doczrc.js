304523dadef0616b25c5737ea561ba3b29e0577f
Weslei Juan Novaes Pereira <wesleimsr@gmail.com>
2019-10-31 21:02:12 -0300

    Added docs

---
+++ doczrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  title: "useDataStructures",
+  typescript: true,
+  themeConfig: {
+    mode: 'dark',
+    colors: {
+      primary: "#3BCCA5",
+    },
+    linesToScrollEditor: 100
+  }
+}
