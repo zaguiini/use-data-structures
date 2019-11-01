059bbc3434c80edf3278885821e2f10c16872a54
Weslei Juan Novaes Pereira <wesleimsr@gmail.com>
2019-11-01 09:58:30 -0300

    Chore: Docs (#36)
    * Added docs
    
    * Update readme
    
    * removed unused folder
    
    * Updated docs

---
+++ doczrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  title: 'useDataStructures',
+  typescript: true,
+  menu: ['useMap', 'useSet', 'Sorting algorithms'],
+  themeConfig: {
+    mode: 'dark',
+    colors: {
+      primary: "#3BCCA5",
+    },
+    linesToScrollEditor: 100
+  }
+}
